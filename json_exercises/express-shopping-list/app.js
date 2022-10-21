2511b6845c28553fb90b54cf51275fedfb746b9b
radnorio <radnorgfowler@gmail.com>
2022-10-21 13:10:36 -0600

    added shopping
    this is a merge for the solution

---
+++ json_exercises/express-shopping-list/app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express();
+const itemsRoutes = require("./routes/items")
+const ExpressError = require("./expressError")
+
+
+app.use(express.json());
+app.use("/items", itemsRoutes);
+
+//404
+
+app.use(function (req, res, next) {
+  return new ExpressError("Not Found", 404);
+});
+
+//handles errors
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
+
+module.exports = app
---
+++ json_exercises/express-shopping-list/routes/items.js
@@ -0,0 +1,65 @@
+const Item = require('../item');
+const express = require('express');
+
+const router = express.Router();
+
+//get route for item object
+
+router.get('', (req, res, next) => {
+  try {
+    return res.json({ items: Item.findAll() });
+  } 
+  catch(err){
+    return next(err)
+  }
+});
+
+// post route for item
+
+router.post('', (req, res, next) => {
+  try {
+    let newItem = new Item(req.body.name, req.body.price);
+    return res.json({item: newItem});
+  } 
+  catch (err) {
+    return next(err)
+  }
+});
+
+//get name of item
+
+router.get('/:name', (req, res, next) => {
+  try {
+    let foundItem = Item.find(req.params.name);
+    return res.json({item:foundItem});
+  } 
+  catch(err){
+    return next(err)
+  }
+});
+
+// changes name of item
+
+router.patch('/:name', (req, res, next) => {
+  try {
+    let foundItem = Item.update(req.params.name, req.body);
+    return res.json({ item: foundItem });
+  } 
+  catch (err) {
+    return next(err)
+  }
+});
+
+// delete item
+
+router.delete('/:name', (req, res, next) => {
+  try {
+    Item.remove(req.params.name);
+    return res.json({message:'Deleted'});
+  } 
+  catch (err) {
+    return next(err)
+  }
+});
+
+module.exports = router;
